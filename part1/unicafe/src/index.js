fedbe36a7834fc5d28bc0234df063ef15f1cb639
kendellc <kendell@gmail.com>
2020-04-10 22:27:02 -0700

    Exercise 1.8

--- part1/unicafe/src/index.js
+++ part1/unicafe/src/index.js
@@ -1,33 +1,38 @@
 import React, { useState } from 'react'
 import ReactDOM from 'react-dom'
 
+const Statistics = (props) => {
+    let all = props.good + props.neutral + props.bad;
+    return (
+        <>
+            <h1>{"statistics"}</h1>
+            <div><strong>{"good"} {props.good}</strong></div>
+            <div><strong>{"neutral"} {props.neutral}</strong></div>
+            <div><strong>{"bad"} {props.bad}</strong></div>
+            <div><strong>{"all"} {all}</strong></div>
+            <div><strong>{"average"} {all === 0 ? 0 : (props.good - props.bad) / all}</strong></div>
+            <div><strong>{"positive"} {all === 0 ? 0 : props.good / all * 100} %</strong></div>
+        </>
+    )
+}
+
 const App = () => {
     // save clicks of each button to own state
     const [good, setGood] = useState(0)
     const [neutral, setNeutral] = useState(0)
     const [bad, setBad] = useState(0)
-    let all;
-
-    const setAll = () => {
-        all = good + neutral + bad
-    }
 
     const handleGoodClick = () => {
         setGood(good + 1)
-        setAll()
     }
 
     const handleNeutralClick = () => {
         setNeutral(neutral + 1)
-        setAll()
     }
 
     const handleBadClick = () => {
         setBad(bad + 1)
-        setAll()
     }
-
-    setAll();
 
     return (
         <div>
@@ -35,13 +40,7 @@ const App = () => {
             <button onClick={handleGoodClick}>{"good"}</button>
             <button onClick={handleNeutralClick}>{"neutral"}</button>
             <button onClick={handleBadClick}>{"bad"}</button>
-            <h1>{"statistics"}</h1>
-            <div><strong>{"good"} {good}</strong></div>
-            <div><strong>{"neutral"} {neutral}</strong></div>
-            <div><strong>{"bad"} {bad}</strong></div>
-            <div><strong>{"all"} {all}</strong></div>
-            <div><strong>{"average"} {all == 0 ? 0 : (good - bad) / all}</strong></div>
-            <div><strong>{"positive"} {all == 0 ? 0 : good / all * 100} %</strong></div>
+            <Statistics good={good} neutral={neutral} bad={bad} />
         </div>
     )
 }
