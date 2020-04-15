f50822973ee9e212b0fe12dcc02c6afc5cad2489
kendellc <kendell@gmail.com>
2020-04-14 18:50:19 -0700

    Exercise 2.9

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,19 +1,27 @@
 import React, { useState } from 'react'
 
-const Persons = ({ persons }) => {
+const Persons = ({ persons, filter }) => {
     return (
         <ul>
-            {persons.map(person => <li key={person.name}>{person.name} {person.number}</li>)}
+            {
+                persons
+                    .filter(person => person.name.toLowerCase().includes(filter.toLowerCase()))
+                    .map(person => <li key={person.name}>{person.name} {person.number}</li>)
+            }
         </ul>
     )
 }
 
 const App = () => {
     const [persons, setPersons] = useState([
-        { name: 'Arto Hellas', number: '33-44-567890' }
+        { name: 'Arto Hellas', number: '33-44-567890' },
+        { name: 'Ada Lovelace', number: '39-44-5323523' },
+        { name: 'Dan Abramov', number: '12-43-234345' },
+        { name: 'Mary Poppendieck', number: '39-23-6423122' }
     ])
     const [newName, setNewName] = useState('')
     const [newNumber, setNewNumber] = useState('')
+    const [filter, setFilter] = useState('')
 
     const addName = (event) => {
         event.preventDefault()
@@ -42,9 +50,16 @@ const App = () => {
         setNewNumber(event.target.value)
     }
 
+    const handleFilterChange = (event) => {
+        setFilter(event.target.value)
+    }
+
     return (
         <div>
             <h2>Phonebook</h2>
+            <p>
+                filter shown as <input value={filter} onChange={handleFilterChange} />
+            </p>
             <form onSubmit={addName}>
                 <div>
                     name: <input value={newName} onChange={handleNameChange} />
@@ -57,7 +72,7 @@ const App = () => {
                 </div>
             </form>
             <h2>Numbers</h2>
-            <Persons persons={persons} />
+            <Persons persons={persons} filter={filter} />
         </div>
     )
 }
