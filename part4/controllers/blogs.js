6299b8bd8a28d4de58cc541dadf56be36af88441
kendellc <kendell@gmail.com>
2020-05-17 16:33:18 -0700

    Exercise 4.17

--- part4/controllers/blogs.js
+++ part4/controllers/blogs.js
@@ -1,8 +1,9 @@
 const blogsRouter = require('express').Router()
 const Blog = require('../models/blog')
+const User = require('../models/user')
 
 blogsRouter.get('/', async (request, response) => {
-    const blogs = await Blog.find({})
+    const blogs = await Blog.find({}).populate('user', { username: 1, name: 1 })
     response.json(blogs)
 })
 
@@ -13,17 +14,23 @@ blogsRouter.post('/', async (request, response) => {
         response.status(400).end()
     }
 
+    const user = await User.findOne({})
+
     const newBlog = {
         title,
         author,
         url,
-        likes: likes || 0
+        likes: likes || 0,
+        user: user._id
     }
 
     const blog = new Blog(newBlog)
 
-    const result = await blog.save()
-    response.json(result.toJSON())
+    const savedBlog = await blog.save()
+    user.blogs = user.blogs.concat(savedBlog._id)
+    await user.save()
+     
+    response.json(savedBlog.toJSON())
 })
 
 blogsRouter.delete('/:id', async (request, response) => {
--- part4/controllers/users.js
+++ part4/controllers/users.js
@@ -3,20 +3,15 @@ const usersRouter = require('express').Router()
 const User = require('../models/user')
 
 usersRouter.get('/', async (request, response) => {
-  const users = await User.find({})
+  const users = await User
+      .find({}).populate('blogs', {
+          title: 1,
+          author: 1,
+          url: 1,
+          likes: 1
+      })
   response.json(users.map(u => u.toJSON()))
 })
-
-// usersRouter.get('/', async (request, response) => {
-//   const users = await User
-//       .find({}).populate('blogs', {
-//           title: 1,
-//           author: 1,
-//           url: 1,
-//           likes: 1
-//       })
-//   response.json(users.map(u => u.toJSON()))
-// })
 
 usersRouter.post('/', async (request, response) => {
   const body = request.body
