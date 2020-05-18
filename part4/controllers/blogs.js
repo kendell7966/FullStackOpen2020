05e9d333e7e39354030203add8056133bedd6f20
kendellc <kendell@gmail.com>
2020-05-18 10:03:07 -0700

    Exercise 4.20

--- part4/controllers/blogs.js
+++ part4/controllers/blogs.js
@@ -8,14 +8,6 @@ blogsRouter.get('/', async (request, response) => {
     response.json(blogs)
 })
 
-const getTokenFrom = request => {
-    const authorization = request.get('authorization')
-    if (authorization && authorization.toLowerCase().startsWith('bearer ')) {
-        return authorization.substring(7)
-    }
-    return null
-}
-
 blogsRouter.post('/', async (request, response) => {
     const { title, author, url, likes } = request.body
 
@@ -23,7 +15,7 @@ blogsRouter.post('/', async (request, response) => {
         response.status(400).end()
     }
 
-    const token = getTokenFrom(request)
+    const token = request.token
     const decodedToken = jwt.verify(token, process.env.SECRET)
     if (!token || !decodedToken.id) {
         return response.status(401).json({ error: 'token missing or invalid' })
